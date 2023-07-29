dea9df2d4db22ef95fb3b7ba563e87b6e87a5513
scootmatt <scootmatt@hotmail.com>
2023-07-29 17:44:45 +0100

    polygons class

---
+++ ts/polygon.ts
@@ -0,0 +1,21 @@
+const COLORS = [ "#4B0082", "#250041", "#120020" ]
+
+export class Star {
+    x: number;
+    y: number;
+    sides: number;
+    size: number;
+    color: number;
+
+    constructor(x: number, y: number, sides: number, size: number) {
+        this.x = x
+        this.y = y
+        this.sides = sides
+        this.size = size
+        this.color = Math.floor(Math.random() * COLORS.length)
+    }
+
+    getColorHex = (): string => {
+        return COLORS[this.color]
+    }
+}
