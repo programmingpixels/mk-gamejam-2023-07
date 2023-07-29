bb98008742426af84a998e6c823c71a330891ce0
scootmatt <scootmatt@hotmail.com>
2023-07-29 14:39:20 +0100

    add a title screen whoop

--- js/canvas.js
+++ js/canvas.js
@@ -37,3 +37,50 @@ const paintBack = () => {   // draw the background color
 const initialiseBackCanvas = () => {
 
 }
+
+class Star {
+    constructor(){
+    this.star_colors = [ "#010101", "#232323", "#454545", "#676767", "#898989", "#ABABAB", "#CDCDCD", "#EFEFEF", "#FFFFFF",
+                         "#EFEFEF", "#CDCDCD", "#ABABAB", "#898989", "676767", "454545", "232323" ];
+    this.x = Math.floor(Math.random() * config.canvasWidth);
+    this.y = Math.floor(Math.random() * config.canvasHeight);
+    this.persistence = Math.floor((Math.random() * 100) + 60);
+    this.decay = this.persistence;
+    this.color = Math.floor( Math.random() * this.star_colors.length );
+    }
+
+    update = () => {
+        this.decay -= 1;
+        if ( this.decay <= 0 ) {
+            this.decay = this.persistence;
+            this.color += 1;
+            if ( this.color > (this.star_colors.length - 1) ) {
+                this.color = 0;
+            }
+        }
+    }
+
+    paint = () => {
+        frontCanvas.ctx.fillStyle = this.star_colors[(this.color)];
+        frontCanvas.ctx.fillRect(this.x, this.y, 1, 1);
+    }
+}
+
+class TitleScreen {
+    constructor() {
+        this.numOfStars = 20;
+        this.stars = [];
+        for (let i = 0; i < this.numOfStars; i++) {
+            const s = new Star();
+            this.stars.push(s);
+        };
+        console.log(this.stars);
+    }
+
+    paintStars = () => {
+        for (let i = 0; i < this.numOfStars; i++) {
+            this.stars[i].paint();
+            this.stars[i].update();
+        };
+    }
+};
