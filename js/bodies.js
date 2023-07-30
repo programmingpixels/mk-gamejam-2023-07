e8f04cb1d2e198dd1eee6aaba3307692934f1c15
Danny Barthaud <danny.barthaud@habitat.energy>
2023-07-30 16:28:08 +0100

    Add forces

--- js/bodies.js
+++ js/bodies.js
@@ -1,13 +1,13 @@
 import { Polygon } from "./polygon.js";
-import { toDegrees } from "./utils.js";
+import { toRadians } from "./utils.js";
 import { randomIntegerInclusive } from "./utils.js";
 export class Body {
     constructor(x, y, color, velocity, mass, polygon) {
-        this.applyForce = (direction, magnitude, timeDelta) => {
-            const f_x = magnitude * Math.sin(toDegrees(direction));
-            const f_y = magnitude * Math.cos(toDegrees(direction));
+        this.applyForce = (direction_d, magnitude, timeDelta) => {
+            const f_x = magnitude * Math.sin(toRadians(direction_d));
+            const f_y = magnitude * Math.cos(toRadians(direction_d));
             this.velocity.x += timeDelta * f_x / this.mass;
-            this.velocity.y += timeDelta * f_y / this.mass;
+            this.velocity.y += -timeDelta * f_y / this.mass;
         };
         this.x = x;
         this.y = y;
@@ -20,14 +20,14 @@ export class Body {
 }
 export class Obstacle extends Body {
     constructor() {
-        const polygon = new Polygon(0, 50, "#0000FF", "#000000", true, true, true, true);
+        const polygon = new Polygon(0, 50, "#0000FF", "#000000", true, true, true, false);
         super(100, 100, 50, { x: 0, y: 0 }, 0, polygon);
     }
 }
 export class Enemy extends Body {
     constructor() {
         let health = randomIntegerInclusive(3, 8);
-        const polygon = new Polygon(health, 20, "#00FF00", "#000000", true, true, true, true);
+        const polygon = new Polygon(health, 20, "#00FF00", "#000000", true, true, true, false);
         super(200, 200, 50, { x: 0, y: 0 }, 0, polygon);
         this.health = health;
     }
@@ -35,24 +35,24 @@ export class Enemy extends Body {
 export class Player extends Body {
     constructor(health = 5) {
         const polygon = new Polygon(3, 10, "#FF0000", "#000000", true, true, true, true);
-        super(50, 50, 50, { x: 0, y: 0 }, 0, polygon);
+        super(50, 50, 50, { x: 0, y: 0 }, 100, polygon);
         this.update = (timeDelta, inputState) => {
             // rotate
             if (inputState.left) {
-                this.rotation -= 1;
+                this.rotation -= 2;
             }
             if (inputState.right) {
-                this.rotation += 1;
+                this.rotation += 2;
             }
             if (inputState.up) {
-                this.applyForce(this.rotation, 5, timeDelta);
+                this.applyForce(this.rotation, 0.005, timeDelta);
             }
             if (inputState.down) {
-                this.applyForce(this.rotation, -5, timeDelta);
+                this.applyForce(this.rotation, -0.005, timeDelta);
             }
             // update position
-            this.x = this.x + this.velocity.x * timeDelta;
-            this.y = this.y + this.velocity.y * timeDelta;
+            this.x += this.velocity.x * timeDelta;
+            this.y += this.velocity.y * timeDelta;
         };
         this.health = health;
     }
--- ts/bodies.ts
+++ ts/bodies.ts
@@ -1,6 +1,6 @@
 import { InputState } from "./inputs.js"
 import { Polygon } from "./polygon.js"
-import { toDegrees } from "./utils.js"
+import { toDegrees, toRadians } from "./utils.js"
 import { randomIntegerInclusive } from "./utils.js"
 
 export interface Velocity {
@@ -27,12 +27,12 @@ export class Body {
         this.polygon = polygon
     }
 
-    applyForce = (direction: number, magnitude: number, timeDelta: number) => {
-        const f_x = magnitude * Math.sin(toDegrees(direction))
-        const f_y = magnitude * Math.cos(toDegrees(direction))
+    applyForce = (direction_d: number, magnitude: number, timeDelta: number) => {
+        const f_x = magnitude * Math.sin(toRadians(direction_d))
+        const f_y = magnitude * Math.cos(toRadians(direction_d))
 
         this.velocity.x += timeDelta * f_x / this.mass
-        this.velocity.y += timeDelta * f_y / this.mass
+        this.velocity.y += - timeDelta * f_y / this.mass
     }
 }
 
@@ -40,7 +40,7 @@ export class Body {
 
 export class Obstacle extends Body {
     constructor() {
-        const polygon = new Polygon(0, 50, "#0000FF", "#000000", true, true, true, true)
+        const polygon = new Polygon(0, 50, "#0000FF", "#000000", true, true, true, false)
 
         super(100, 100, 50, { x: 0, y: 0 }, 0, polygon)
     }
@@ -51,7 +51,7 @@ export class Enemy extends Body {
 
     constructor() {
         let health = randomIntegerInclusive(3, 8)
-        const polygon = new Polygon(health, 20, "#00FF00", "#000000", true, true, true, true)
+        const polygon = new Polygon(health, 20, "#00FF00", "#000000", true, true, true, false)
         super(200, 200, 50, { x: 0, y: 0 }, 0, polygon)
 
         this.health = health
@@ -64,28 +64,28 @@ export class Player extends Body {
     constructor(health: number = 5) {
         const polygon = new Polygon(3, 10, "#FF0000", "#000000", true, true, true, true)
 
-        super(50, 50, 50, { x: 0, y: 0 }, 0, polygon)
+        super(50, 50, 50, { x: 0, y: 0 }, 100, polygon)
         this.health = health
     }
 
     update = (timeDelta: number, inputState: InputState) => {
         // rotate
         if (inputState.left) {
-            this.rotation -= 1
+            this.rotation -= 2
         }
         if (inputState.right) {
-            this.rotation += 1
+            this.rotation += 2
         }
         if (inputState.up) {
-            this.applyForce(this.rotation, 5, timeDelta)
+            this.applyForce(this.rotation, 0.005, timeDelta)
         }
         if (inputState.down) {
-            this.applyForce(this.rotation, -5, timeDelta)
+            this.applyForce(this.rotation, -0.005, timeDelta)
         }
 
         // update position
-        this.x = this.x + this.velocity.x * timeDelta
-        this.y = this.y + this.velocity.y * timeDelta
+        this.x += this.velocity.x * timeDelta
+        this.y += this.velocity.y * timeDelta
     }
 }
 
