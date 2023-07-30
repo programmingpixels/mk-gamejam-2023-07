77292a9cb86da5f92782f7808bdf1fc162f1323a
Danny (on Mint19) <danny.barthaud@open.ac.uk>
2023-07-30 11:34:07 +0100

    basic polygon constructor

--- ts/polygon.ts
+++ ts/polygon.ts
@@ -1,21 +1,32 @@
-const COLORS = [ "#4B0082", "#250041", "#120020" ]
 
 export class Polygon {
     x: number;
     y: number;
     sides: number;
-    size: number;
-    color: number;
+    radius: number;
+    hitRadius: number;
+    showHitRadius: boolean;
+    isFilled: boolean;
+    fillColor: string;
+    isOutlined: boolean;
+    outlineColor: string;
+    rotation: number;
+    isPlayer: boolean;
 
-    constructor(x: number, y: number, sides: number, size: number) {
+    constructor(x: number, y: number, sides: number, radius: number, fillColor: string, outlineColor: string, rotation: number,
+                showHitRadius: boolean=false, isFilled: boolean=true, isOutlined: boolean=false, isPlayer: boolean) {
         this.x = x
         this.y = y
         this.sides = sides
-        this.size = size
-        this.color = Math.floor(Math.random() * COLORS.length)
+        this.radius = radius
+        this.fillColor = fillColor
+        this.outlineColor = outlineColor
+        this.rotation = rotation
+        this.showHitRadius = showHitRadius
+        this.isFilled = isFilled
+        this.isOutlined = isOutlined
+        this.isPlayer = isPlayer
+        this.hitRadius = this.radius
     }
 
-    getColorHex = (): string => {
-        return COLORS[this.color]
-    }
 }
