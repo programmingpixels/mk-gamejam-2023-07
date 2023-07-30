c756e1f785827ac7aa70e3e983f9d83681b05ecc
Danny (on Mint19) <danny.barthaud@open.ac.uk>
2023-07-30 17:54:39 +0100

    d

--- js/applicationState.js
+++ js/applicationState.js
@@ -3,14 +3,20 @@ import { TitlePage } from "./titlePage.js";
 import { Renderer } from "./renderer.js";
 export class ApplicationState {
     constructor() {
+        this.freeze = false;
         this.update = (inputStates) => {
+            if (this.freeze) {
+                return;
+            }
             if (this.titlePage) {
                 if (!this.titlePage.update(inputStates)) {
                     this.startGame();
                 }
             }
             else if (this.gameState) {
-                this.gameState.update(inputStates);
+                if (!this.gameState.update(inputStates)) {
+                    this.freeze = true;
+                }
             }
         };
         this.startGame = () => {
@@ -37,7 +43,7 @@ export class ApplicationState {
             }
             this.renderer.paintTitle("bold 36px Courier", "#11FF11");
             this.renderer.paintPressStartGame("bold 28px Courier", this.titlePage.stars[0].getColorHex());
-            this.renderer.paintPolygon(this.titlePage.polygon, 500, 500, 11);
+            this.renderer.paintPolygon(this.titlePage.polygon, 500, 300, 11);
         };
         this.paintGameState = () => {
             var _a, _b, _c;
@@ -45,14 +51,21 @@ export class ApplicationState {
             (_a = this.gameState) === null || _a === void 0 ? void 0 : _a.players.forEach(player => {
                 this.renderer.paintPolygon(player.polygon, player.x, player.y, player.rotation);
             });
-            // paint enemies
-            (_b = this.gameState) === null || _b === void 0 ? void 0 : _b.enemies.forEach(enemy => {
-                this.renderer.paintPolygon(enemy.polygon, enemy.x, enemy.y, enemy.rotation);
-            });
+            // // paint enemies
+            // this.gameState?.enemies.forEach(enemy => {
+            //     this.renderer.paintPolygon(
+            //         enemy.polygon,
+            //         enemy.x,
+            //         enemy.y,
+            //         enemy.rotation,
+            //     )
+            // })
             // paint obstacles
-            (_c = this.gameState) === null || _c === void 0 ? void 0 : _c.obstacles.forEach(obstacle => {
+            (_b = this.gameState) === null || _b === void 0 ? void 0 : _b.obstacles.forEach(obstacle => {
                 this.renderer.paintPolygon(obstacle.polygon, obstacle.x, obstacle.y, obstacle.rotation);
             });
+            // paint score
+            this.renderer.paintScore((_c = this.gameState) === null || _c === void 0 ? void 0 : _c.score);
         };
         this.renderer = new Renderer();
         this.titlePage = new TitlePage(this.renderer.visibleCanvas.width, this.renderer.visibleCanvas.height);
